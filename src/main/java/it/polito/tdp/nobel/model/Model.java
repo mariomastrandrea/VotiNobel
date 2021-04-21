680bcd95ef59b428c597f479501404450dfab7f1
mariomastrandrea <mariomastrandrea@mbp-di-mario.homenet.telecomitalia.it>
2021-04-21 15:23:01 +0200

    modifiche di ottimizzazione ai metodi 'cercaRicorsivamente_()'
    - cercaRicorsivamente1() : rappresenta il metodo iniziale, senza
    ottimizzazioni
    
    - cercaRicorsivamente2() : rappresenta il metodo con ricerca dicotomica
    -> prestazioni migliori
    
    - cercaRicorsivamente3() : rappresenta il metodo più ottimizzato ->
    prestazioni migliori in assoluto

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -20,7 +20,6 @@ public class Model
 		this.insiemeEsamiDiPartenza = dao.getTuttiEsami();
 	}
 	
-	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) 
 	{
 		Set<Esame> soluzioneParziale = new HashSet<>();
@@ -28,13 +27,17 @@ public class Model
 		this.mediaSoluzioneMigliore = 0.0;
 		this.creditiTotali = numeroCrediti;
 		
-		cercaRicorsivamente3(soluzioneParziale, 0);
+		cercaRicorsivamente3(soluzioneParziale, 0); //<-- *** QUI *** per cambiare il metodo della ricorsione
 		
 		return this.soluzioneMigliore;	
 	}
 
-	
 	@SuppressWarnings("unused")
+	/**
+	 * metodo iniziale NON ottimizzato, prestazioni peggiori (per 35cfu ~ t=406ms)
+	 * @param soluzioneParziale
+	 * @param L
+	 */
 	private void cercaRicorsivamente1(Set<Esame> soluzioneParziale, int L)
 	{
 		//casi terminali
@@ -74,6 +77,12 @@ public class Model
 		
 	}
 	
+	/**
+	 * metodo semi-ottimizzato, con ricerca dicotomica; prestazioni molto migliori 
+	 * 					(per 35cfu ~ t=6ms;  per 70cfu ~ t=145ms)
+	 * @param soluzioneParziale
+	 * @param L
+	 */
 	@SuppressWarnings("unused")
 	private void cercaRicorsivamente2(Set<Esame> soluzioneParziale, int L)
 	{
@@ -111,6 +120,12 @@ public class Model
 		cercaRicorsivamente2(soluzioneParziale, L+1);
 	}
 	
+	/**
+	 * >>> metodo ottimizzato, con l'uso di Set. Prestazioni *** MIGLIORI *** in assoluto
+	 * 					(per 35cfu ~ t=4ms;  per 70cfu ~ t=106ms)
+	 * @param soluzioneParziale 
+	 * @param L livello della ricorsione corrente
+	 */
 	private void cercaRicorsivamente3(Set<Esame> soluzioneParziale, int L)
 	{
 		//casi terminali
@@ -141,14 +156,72 @@ public class Model
 		for(int i=L; i<insiemeEsamiDiPartenza.size(); i++)
 		{
 			Esame e = this.insiemeEsamiDiPartenza.get(i);
-			soluzioneParziale.add(e);
-			cercaRicorsivamente3(soluzioneParziale, L+1);
-			soluzioneParziale.remove(e);
+			if(!soluzioneParziale.contains(e))
+			{
+				soluzioneParziale.add(e);
+				cercaRicorsivamente3(soluzioneParziale, i+1);	//// <--- passo 'i+1' e non 'L+1', 
+																//perché i+1 rappresenta REALMENTE il livello
+																//della prossima iterazione (* vedi esempio sotto)
+				soluzioneParziale.remove(e); //backtracking
+			}
 		}
 		
 	}
-
-
+	
+	
+	/* esempio (SENZA ottimizzazione):
+	 * partenza = {e1, e2, e3, e4, ...}
+	 * 
+	 *  L=0
+	 * parziale = {e1}
+	 * 		- parziale = {e1, e2}
+	 * 	 	  L=1
+	 * 		  [...]
+	 * 
+	 *		- parziale = {e1, e3}
+	 *	  	  L=1 
+	 * 		  [...]
+	 * 
+	 *  L=0
+	 * parziale = {e2}
+	 * 		- parziale = {e2, e1} !!! problema -> il livello era L=0, quindi il 
+	 * 			L=1								metodo RI-SELEZIONA e1, che è l'esame in posizione 0 
+	 * 			[...]						In realtà dovrebbe selezionare e3. Che è l'elemento in posizione 2
+	 * 									>> Quindi: non siamo al livello 0, ma al livello 1 (!)
+	 * 
+	 * SOLUZIONE
+	 * 
+	 * esempio (CON ottimizzazione):
+	 * partenza = {e1, e2, e3, e4, ...}
+	 * 
+	 *  L=0
+	 * parziale = {e1}
+	 * 		- parziale = {e1, e2}
+	 * 		  L=1 
+	 * 		  [...]
+	 * 
+	 * 		- parziale = {e1, e3}
+	 * 		  L=2 (!!!)
+	 * 		  [...]
+	 * 
+	 * L=1 !!!  <---- NON L=0			{ Qui il livello aumenta progressivamente ad ogni ciclo }
+	 * parziale = {e2}
+	 * 		- parziale = {e2, e3} !!! 
+	 * 		  L=2
+	 * 		  [...]
+	 * 
+	 * 		- parziale = {e2, e4} 
+	 * 		  L=3   (!)
+	 * 		  [...]
+	 * 
+	 * 		- parziale = {e2, e5}
+	 * 	  	  L=4	(!)
+	 * 		  [...]
+	 * 
+	 * [...]
+	 */
+	
+	
 	public double calcolaMedia(Set<Esame> esami) 
 	{
 		
